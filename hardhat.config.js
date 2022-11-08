7431eaf4868c794ac13a8ebb9ca2474f39769e14
dov <benscheinberg@gmail.com>
2022-11-08 08:00:58 -0500

    got contracts to compile

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
+require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-
       url: process.env.NODE_ENDPOINT,
       accounts: [process.env.PRIVATE_KEY]
     }
