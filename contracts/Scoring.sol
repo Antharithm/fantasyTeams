8a610c9a8ab0620525ed199693ff80e4d7eb18f8
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-11-12 16:13:13 -0600

    uint256 >> uint

--- contracts/Scoring.sol
+++ contracts/Scoring.sol
@@ -5,79 +5,80 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
 // this is an example truflation contract, it needs to be updated to use the
-// sports data api. 
+// sports data api.
 
 contract TruflationTester is ChainlinkClient, ConfirmedOwner {
-  using Chainlink for Chainlink.Request;
+    using Chainlink for Chainlink.Request;
 
-  string public yoyInflation;
-  address public oracleId;
-  string public jobId;
-  uint256 public fee;
+    string public yoyInflation;
+    address public oracleId;
+    string public jobId;
+    uint public fee;
 
-  // Please refer to
-  // https://github.com/truflation/quickstart/blob/main/network.md
-  // for oracle address. job id, and fee for a given network
+    // Please refer to
+    // https://github.com/truflation/quickstart/blob/main/network.md
+    // for oracle address. job id, and fee for a given network
 
-  constructor(
-    address oracleId_,
-    string memory jobId_,
-    uint256 fee_
-  ) ConfirmedOwner(msg.sender) {
-    setPublicChainlinkToken();
+    constructor(
+        address oracleId_,
+        string memory jobId_,
+        uint fee_
+    ) ConfirmedOwner(msg.sender) {
+        setPublicChainlinkToken();
 
-    // use this for Goerli (chain: 5)
-    // setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        // use this for Goerli (chain: 5)
+        // setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
 
-    oracleId = oracleId_;
-    jobId = jobId_;
-    fee = fee_;
-  }
+        oracleId = oracleId_;
+        jobId = jobId_;
+        fee = fee_;
+    }
 
+    function requestYoyInflation() public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            bytes32(bytes(jobId)),
+            address(this),
+            this.fulfillYoyInflation.selector
+        );
+        req.add("service", "truflation/current");
+        req.add("keypath", "yearOverYearInflation");
+        req.add("abi", "json");
+        return sendChainlinkRequestTo(oracleId, req, fee);
+    }
 
-  function requestYoyInflation() public returns (bytes32 requestId) {
-    Chainlink.Request memory req = buildChainlinkRequest(
-      bytes32(bytes(jobId)),
-      address(this),
-      this.fulfillYoyInflation.selector
-    );
-    req.add("service", "truflation/current");
-    req.add("keypath", "yearOverYearInflation");
-    req.add("abi", "json");
-    return sendChainlinkRequestTo(oracleId, req, fee);
-  }
+    function fulfillYoyInflation(bytes32 _requestId, bytes memory _inflation)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        yoyInflation = string(_inflation);
+    }
 
-  function fulfillYoyInflation(
-    bytes32 _requestId,
-    bytes memory _inflation
-  ) public recordChainlinkFulfillment(_requestId) {
-    yoyInflation = string(_inflation);
-  }
+    function changeOracle(address _oracle) public onlyOwner {
+        oracleId = _oracle;
+    }
 
-  function changeOracle(address _oracle) public onlyOwner {
-    oracleId = _oracle;
-  }
+    function changeJobId(string memory _jobId) public onlyOwner {
+        jobId = _jobId;
+    }
 
-  function changeJobId(string memory _jobId) public onlyOwner {
-    jobId = _jobId;
-  }
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
 
-  function getChainlinkToken() public view returns (address) {
-    return chainlinkTokenAddress();
-  }
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
 
-  function withdrawLink() public onlyOwner {
-    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-    require(link.transfer(msg.sender, link.balanceOf(address(this))),
-    "Unable to transfer");
-  }
-
-/*
+    /*
   // The following are for retrieving inflation in terms of wei
   // This is useful in situations where you want to do numerical
   // processing of values within the smart contract
 
-  // This will require a int256 rather than a uint256 as inflation
+  // This will require a int256 rather than a uint as inflation
   // can be negative
 
   int256 public inflationWei;
