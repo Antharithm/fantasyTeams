f22aebae588c9f1bf7fa6ad9afa23a824d43d96c
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-11-12 20:19:46 -0600

    Template test

---
+++ test/Template.test.js
@@ -0,0 +1,32 @@
+const { expect, assert } = require('chai');
+
+const uint = (num) => parseInt(num.toString());
+const uintArray = (nums) => nums.map((num) => uint(num));
+
+describe('TestContract', function () {
+  beforeEach(async () => {});
+
+  describe('Operation', function () {
+    beforeEach(async () => {});
+    it('Should let people mint', async () => {});
+    it('Should let people mint', async () => {});
+  });
+
+  describe('Operation', function () {
+    beforeEach(async () => {});
+    it('Should let people mint', async () => {});
+    it('Should let people mint', async () => {});
+  });
+
+  describe('Operation', function () {
+    beforeEach(async () => {});
+    it('Should let people mint', async () => {});
+    it('Should let people mint', async () => {});
+  });
+
+  describe('Operation', function () {
+    beforeEach(async () => {});
+    it('Should let people mint', async () => {});
+    it('Should let people mint', async () => {});
+  });
+});
